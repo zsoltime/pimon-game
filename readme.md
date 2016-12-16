857319b6c39aecefa9ebd5d293663139ccc8b9af
Zsolt Meszaros <zsolt@sowad.com>
2016-12-16 04:30:12 +0000

    Initial commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Day 13 of My 3 Weeks Game Challenge
+
+Pimon Game
+
+## User Stories
+
+- [ ]
+
+## Next Steps
---
+++ src/js/game.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function Game() {
+  function init() {}
+}
+
+document.addEventListener("DOMContentLoaded", function(e) {
+  Game.init();
+});
